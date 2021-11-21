97ba4fb2bcc75313bda9a941243c0a2a8f896fc7
Duke Manh <manhducdkcb@gmail.com>
2021-11-21 22:16:46 +0000

    Github Action

--- index.ts
+++ index.ts
@@ -1,5 +1,15 @@
-const greeting = 'Hello world';
+import * as core from '@actions/core';
+import * as github from '@actions/github';
 
-const x = () => console.log(greeting);
-
-x();
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  const time = new Date().toTimeString();
+  core.setOutput('time', time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed('Something went wrong');
+}
