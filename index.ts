6a043b9c575ceb40a912433007c6438ab494a0b5
Duke Manh <manhducdkcb@gmail.com>
2021-11-23 22:35:52 +0000

    Scoped-action  prototype

--- index.ts
+++ index.ts
@@ -1,15 +1,21 @@
 import * as core from '@actions/core';
-import * as github from '@actions/github';
+import * as exec from '@actions/exec';
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput('time', time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed('Something went wrong');
+const getCommand = (step: number) => core.getInput(`$${step}`);
+
+async function main() {
+  try {
+    let step = 0;
+    let command = getCommand(step);
+    while (command) {
+      // eslint-disable-next-line no-await-in-loop
+      await exec.getExecOutput(command, []);
+      step += 1;
+      command = getCommand(step);
+    }
+  } catch (error) {
+    core.setFailed((error as Error).message);
+  }
 }
+
+main();
