5fdac05ee9f2c5af0c3928c5c60b45046cede874
Duke Manh <manhducdkcb@gmail.com>
2021-11-25 09:08:01 +0000

    get changed files

---
+++ CONTRIBUTING.md
@@ -0,0 +1,5 @@
+# TO-DO
+
+Test actions
+
+act push -s <GTIHUB_TOKEN> -e act-payload.json
--- index.ts
+++ index.ts
@@ -1,18 +1,22 @@
 import * as core from '@actions/core';
+import { context } from '@actions/github';
 import * as exec from '@actions/exec';
 
-const getCommand = (step: number) => core.getInput(`$${step}`);
+const getCommand = (step: number) => core.getInput(`s${step}`);
 
 async function main() {
   try {
     let step = 0;
     let command = getCommand(step);
+    const changedFiles = core.getInput('files');
+    console.log(changedFiles);
     while (command) {
       // eslint-disable-next-line no-await-in-loop
       await exec.getExecOutput(command, []);
       step += 1;
       command = getCommand(step);
     }
+    console.log(context?.payload.after);
   } catch (error) {
     core.setFailed((error as Error).message);
   }
